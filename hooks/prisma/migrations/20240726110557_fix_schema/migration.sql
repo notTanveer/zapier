a37bd0474ef9389e649a053e8ae6f8d70dd984f8
notTanveer <101289209+notTanveer@users.noreply.github.com>
2024-07-26 16:37:14 +0530

    schema migration

---
+++ hooks/prisma/migrations/20240726110557_fix_schema/migration.sql
@@ -0,0 +1,68 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The `id` column on the `User` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - You are about to drop the `AvailableActions` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `AvailableTriggers` table. If the table is not empty, all the data it contains will be lost.
+  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
+  - Added the required column `userId` to the `Zap` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Action" DROP CONSTRAINT "Action_actionId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Trigger" DROP CONSTRAINT "Trigger_triggerId_fkey";
+
+-- DropIndex
+DROP INDEX "User_email_key";
+
+-- AlterTable
+ALTER TABLE "Action" ADD COLUMN     "metadata" JSONB NOT NULL DEFAULT '{}';
+
+-- AlterTable
+ALTER TABLE "Trigger" ADD COLUMN     "metadata" JSONB NOT NULL DEFAULT '{}';
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "id" SERIAL NOT NULL,
+ALTER COLUMN "name" SET NOT NULL,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "Zap" ADD COLUMN     "userId" INTEGER NOT NULL;
+
+-- DropTable
+DROP TABLE "AvailableActions";
+
+-- DropTable
+DROP TABLE "AvailableTriggers";
+
+-- CreateTable
+CREATE TABLE "AvailableAction" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+
+    CONSTRAINT "AvailableAction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AvailableTrigger" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+
+    CONSTRAINT "AvailableTrigger_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Zap" ADD CONSTRAINT "Zap_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Trigger" ADD CONSTRAINT "Trigger_triggerId_fkey" FOREIGN KEY ("triggerId") REFERENCES "AvailableTrigger"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Action" ADD CONSTRAINT "Action_actionId_fkey" FOREIGN KEY ("actionId") REFERENCES "AvailableAction"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
